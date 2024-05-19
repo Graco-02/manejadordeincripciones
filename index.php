8623cccb6d9721be28e944c68c9664437220622b
graco <fferrer02@hotmail.com>
2024-05-19 11:01:17 -0300

    se crea el formulario modal de inscripciones

--- index.php
+++ index.php
@@ -11,13 +11,15 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/formulario_inscripcion.css">
+    <script src="js/formulario_inscripciones.js"></script>
 </head>
 
 <body>
     <main>
     <section id="menu">
         <ul id="menu_opciones">
-            <li class="opciones_menu" onclick="alert('seleccionado incripciones')">
+            <li class="opciones_menu" onclick="abrir_formulario();">
                 <img src="imagenes/incripcion.png" class="iconos_menu" srcset=""><span>incripcion</span>
             </li>
             <li class="opciones_menu" onclick="alert('ventanilla de pagos')">
@@ -27,7 +29,7 @@
     </section>
 
     <section id="cuerpo">
-        <div id="cabecera">
+        <div id="cabecera" class="cabecera">
             <div id="cabecera_1" class="opciones_cabecera">
                 <img src="imagenes/usuario.png" alt="" srcset="">
                 <div class="cabecera_div">
@@ -53,12 +55,44 @@
         </div>
         <div id="cuerpo_contenido"></div>
     </section>
-    </main>
 
-    <div id="formulario_incripcion" class="cerrado">
+    <section id="formulario_incripcion" class="cerrado">
+          <div id="formulario_inscripcio_cuerpo">
+             <form action="" method="" class="formulario_inscripcion_formulario">
+                <h1>formulario de inscripcion</h1>
 
-    </div>
+                <div>
 
+                      <div class="formulario_inscripcion_formulario_fielset">
+                          <label for="inscripcion_nombres">nombres</label>
+                          <input type="text" name="inscripcion_nombres" id="inscripcion_nombres" required> 
+                      </div>
+                      <br>
+                      <div class="formulario_inscripcion_formulario_fielset">
+                          <label for="inscripcion_apellidos">apellidos</label>
+                          <input type="text" name="inscripcion_apellidos" id="inscripcion_apellidos" required> 
+                      </div>     
+                      <br>
+                      <div class="formulario_inscripcion_formulario_fielset">
+                          <label for="inscripcion_apellidos">identificacion</label>
+                          <input type="text" name="inscripcion_identificacion" id="inscripcion_identificacion" > 
+                      </div>  
+                      <br>
+                      <div class="formulario_inscripcion_formulario_fielset">
+                          <label for="fecha_nacimiento">fecha de nacimiento</label>
+                          <input type="date" name="fecha_nacimiento" id="fecha_nacimiento" required>
+                      </div>  
+                      <br>  <br>
+                      <button id="boton_formulario_inscripcion_envio" class="boton_formulario">enviar</button>
+
+
+                </div>
+
+            </form>
+          </div>
+    </section>
+    
+</main>
 </body>
 
 
---
+++ js/formulario_inscripciones.js
@@ -0,0 +1,4 @@
+function abrir_formulario(){
+  var div_formulario = document.getElementById('formulario_incripcion');
+  div_formulario.classList.toggle('cerrado');
+}
